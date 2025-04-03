94e3cdbda3a309cca087e0bb3935f039dd4c4877
Ryder <rydercasazza@gmail.com>
2025-04-03 14:16:46 -0700

    add tsx support

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -5,6 +5,22 @@ return {
       local lspconfig = require("lspconfig")
 
       lspconfig.typst_lsp.setup({})
+
+      local on_attach = function(client, bufnr)
+        if client.server_capabilities.documentFormattingProvider then
+          vim.api.nvim_create_autocmd("BufWritePre", {
+            buffer = bufnr,
+            callback = function()
+              vim.lsp.buf.formatting_sync()
+            end,
+          })
+        end
+
+      lspconfig.tsserver.setup {
+        on_attach = on_attach,
+        filetypes = { "typescript", "typescriptreact", "typescript.tsx" },
+        cmd = { "typescript-language-server", "--stdio" },
+      }
     end,
   },
 }
